674095c22431a3b32ebed5e1182c37d77f32cc8e
HanzlaAlvi <hanzlaalvi9@gmail.com>
2025-09-05 14:31:37 +0500

    added hello

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <>
       <h1>Online Shopping Store</h1>
       <p>Hi </p>
+      <h3>hello</h3>
       <p>We have a shopping website where you can buy items</p>
       <h2>this site is not portal</h2>
     </>
